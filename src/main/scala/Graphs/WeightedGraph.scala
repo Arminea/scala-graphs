188a9da277d09026e3579f8bbdca57d4e1380c29
Tereza Podavkova <tereza.stanglova@gmail.com>
2020-04-05 16:39:24 +0200

    Adds graph representation and tests

---
+++ src/main/scala/Graphs/WeightedGraph.scala
@@ -0,0 +1,24 @@
+package main.Graphs
+
+case class WeightedEdge[N](destination: N, weight: Int)
+
+class WeightedGraph[N](adjList: Map[N, List[WeightedEdge[N]]]) extends Graph[N] {
+
+  override def nodes: List[N] = adjList.keys.toList
+
+  override def edges: List[(N, N)] = adjList.flatMap {
+    case (node: N, edges: List[WeightedEdge[N]]) => edges.map(edge => (node, edge.destination))
+  }.toList
+
+  def addEdge(from: N, weightedEdge: WeightedEdge[N]): WeightedGraph[N] = {
+    val fromNeighbours = weightedEdge +: adjList.getOrElse(from, Nil)
+    new WeightedGraph(adjList + (from -> fromNeighbours))
+  }
+
+  override def addEdge(from: N, to: N): WeightedGraph[N] = addEdge(from, WeightedEdge(to, 0))
+
+  override def neighbours(node: N): List[N] = adjList.getOrElse(node, Nil).map(_.destination)
+
+  def neighboursWithWeights(node: N): List[WeightedEdge[N]] = adjList.getOrElse(node, Nil).toList
+
+}
