70e58a5a9193eb8da72a96e10bc72e116452eff9
Tereza Podavkova <tereza.stanglova@gmail.com>
2020-04-18 20:44:48 +0200

    Adds floyds algorithm

--- src/main/scala/CycleDetection/CycleDetection.scala
+++ src/main/scala/CycleDetection/CycleDetection.scala
@@ -4,6 +4,13 @@ import main.Graphs.Graph
 
 object CycleDetection {
 
+  /**
+   * Function determines whether the graph contains cycle or not using DFS.
+   *
+   * @param graph   graph
+   * @tparam N
+   * @return        true/false
+   */
   def containsCycleDFS[N](graph: Graph[N]): Boolean = {
 
     case class DfsCycleResult[N](visited: Set[N], isCyclic: Boolean = false)
@@ -27,6 +34,44 @@ object CycleDetection {
     startNodes.isEmpty || startNodes.exists(n => containsCycleDFS0(n).isCyclic)
   }
 
+  /**
+   * Function determines whether the graph contains cycle or not using Floyd's aka. Tortoise and Hare algorithm.
+   *
+   * @param node      starting node
+   * @param graph     graph
+   * @tparam N
+   * @return          true/false
+   */
+  def containsCycleFloyd[N](node: N, graph: Graph[N]): Boolean = {
 
+    case class Race(tortoise: List[N], hare: List[N])
+
+    //move one step
+    def moveOnce(stack: List[N]): List[N] = stack.headOption.map(n => graph.neighbours(n) ++ stack.tail).getOrElse(Nil)
+
+    // compare heads of tortoise and hare stacks
+    def compareHeads(tortoiseHead: Option[N], hareHead: Option[N]): Boolean = (tortoiseHead, hareHead) match {
+      case (Some(t), Some(h)) => t == h
+      case _ => false
+    }
+
+    def containsCycleFloyd0(race: Race): Race = {
+      if (race.hare.isEmpty || compareHeads(race.tortoise.headOption, race.hare.headOption))
+        race
+      else
+        containsCycleFloyd0(Race(
+          moveOnce(race.tortoise),
+          moveOnce(moveOnce(race.hare))
+        ))
+    }
+
+    val race = containsCycleFloyd0(Race(
+      moveOnce(List(node)),
+      moveOnce(moveOnce(List(node)))
+    ))
+
+    compareHeads(race.tortoise.headOption, race.hare.headOption)
+
+  }
 
 }
--- src/test/scala/CycleDetection/CycleDetectionTest.scala
+++ src/test/scala/CycleDetection/CycleDetectionTest.scala
@@ -6,22 +6,35 @@ import org.scalatest.matchers.should.Matchers
 
 class CycleDetectionTest extends AnyFlatSpec with Matchers {
 
-  val bigCycle = Graph[String]
+  val bigCycle = Graph[String]()
     .addEdge("A", "B")
     .addEdge("B", "C")
     .addEdge("C", "D")
     .addEdge("D", "A")
 
-  val withCycle = Graph[String]
+  val withCycle = Graph[String]()
     .addEdge("A", "B")
     .addEdge("B", "C")
     .addEdge("C", "D")
     .addEdge("D", "B")
 
-  val withoutCycle = Graph[String]
+  val withoutCycle = Graph[String]()
     .addEdge("A", "B")
     .addEdge("B", "C")
     .addEdge("B", "D")
+
+  val badForFloyd = Graph[String]()
+    .addEdge("A", "C")
+    .addEdge("A", "B")
+    .addEdge("B", "C")
+    .addEdge("C", "D")
+    .addEdge("D", "E")
+    .addEdge("E", "F")
+    .addEdge("F", "G")
+    .addEdge("G", "H")
+    .addEdge("H", "I")
+    .addEdge("I", "J")
+    .addEdge("J", "K")
 
   "DFS - Graph" should "contain cycle - empty start nodes" in {
     CycleDetection.containsCycleDFS(bigCycle) shouldBe true
@@ -33,6 +46,19 @@ class CycleDetectionTest extends AnyFlatSpec with Matchers {
 
   "DFS - Graph" should "not contain cycle" in {
     CycleDetection.containsCycleDFS(withoutCycle) shouldBe false
+  }
+
+  "Floyd - Graph" should "contain cycle" in {
+    CycleDetection.containsCycleFloyd("A", withCycle) shouldBe true
+  }
+
+  "Floyd - Graph" should "not contain cycle" in {
+    CycleDetection.containsCycleFloyd("A", withoutCycle) shouldBe false
+  }
+
+  "Floyd - Graph" should "contain cycle - false positive" in {
+    // graph is not cyclic, result is false positive
+    CycleDetection.containsCycleFloyd("A", badForFloyd) shouldBe true
   }
 
 }
